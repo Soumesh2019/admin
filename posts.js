6cb03aed95dff96bb7604284114ad9372bafa6d2
Soumesh2019 <soumesh57@gmail.com>
2020-08-17 21:07:03 +0530

    First Commit

--- posts.js
+++ posts.js
@@ -2,8 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require('path');
 const app = express();
-let posts = require(".routes/posts");
-let api = require("./routes/api");
 const Model = require(path.join(__dirname, "./server"));
 app.use(express.static(__dirname + "/public/"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -65,8 +63,6 @@ app.post("/posts/:posts_id", (req, res) => {
       res.send(error);
     }
   });
-
-  // res.end();
 });
 
 app.listen(process.env.PORT || 5000, () => console.log("Server Running on 5000"));
